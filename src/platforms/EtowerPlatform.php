bcc730cff98a2d2d0a2b8240f18dd5f02e7fefe0
dudulu <wack1001@163.com>
2020-04-04 16:41:52 +0800

    临时提交

--- src/platforms/EtowerPlatform.php
+++ src/platforms/EtowerPlatform.php
@@ -41,20 +41,8 @@ class EtowerPlatform extends Platform
      */
     public function getClient()
     {
-        $t = new \DateTime('now');
-
-        $str = "";
-        $key = base64_encode(hash_hmac("sha1", $str, $this->config->get("key"), true));
-
         $headers = [
             'Content-Type' => 'application/json; charset=utf8',
-            'Accept-Language' => 'en-us',
-            'X-WallTech-Date' => $t->format(\DateTime::RFC1123),
-            'Authorization' => sprintf("WallTech %s:%s",
-                $this->config->get("token"),
-                $key
-            ),
-            'Accept' => 'text/json',
         ];
 
         $client = new \GuzzleHttp\Client([
@@ -76,19 +64,25 @@ class EtowerPlatform extends Platform
     }
 
     /**
-     * @inheritDoc
+     * create order
+     *
+     * @param Order $order
+     * @return OrderResult
+     * @throws ExpressException
      */
     public function createOrder(Order $order): OrderResult
     {
         $uri = "/services/shipper/orders";
+        $a = `[{"trackingNo":"ABC123456789001","consignmentId":"1","referenceNo":"20160311001","addressLine1":"200 Bumborah Point Rd","addressLine2":"St. Peters","addressLine3":"","city":"PORT BOTANY","country":"AU","description":"shoes ","nativeDescription":"包含中文字符","email":"test@gmail.com","facility":"230031","instruction":"","invoiceCurrency":"AUD","invoiceValue":100,"phone":"","platform":"","postcode":"2036","recipientCompany":"","recipientName":"Bob","serviceCode":"","serviceOption":"","sku":"T1818ZS39*1","state":"NSW","weightUnit":"","weight":0.776,"dimensionUnit":"","length":0,"width":0,"height":0,"volume":0,"shipperName":"walltech hello","shipperAddressLine1":"pudongxinqu","shipperAddressLine2":"","shipperAddressLine3":"","shipperCity":"","shipperState":"","shipperPostcode":"","shipperCountry":"","shipperPhone":"","recipientTaxId":"","authorityToLeave":"","incoterm":"","lockerService":"","extendData":{"nationalNumber":"","nationalIssueDate":"11/11/2017","cyrillicName":"Bob","imei":"","isImei":true,"vendorid":"64652016681","gstexemptioncode":"","abnnumber":"","sortCode":"","coveramount":12},"orderItems":[{"itemNo":"00001","sku":"T1818ZS39*1-1","description":"PHONE","nativeDescription":"包含中文字符","hsCode":"TT11","originCountry":"CHINA","unitValue":50,"itemCount":1,"weight":0,"productURL":"www.ebay.com"},{"itemNo":"00002","sku":"T1818ZS39*1-2","description":"APPLE","nativeDescription":"包含中文字符","hsCode":"TT11","originCountry":"CHINA","unitValue":25,"itemCount":1,"weight":0,"productURL":"www.ebay.com"},{"itemNo":"00003","sku":"T1818ZS39*1-3","description":"IPAD","nativeDescription":"包含中文字符","hsCode":"TT11","originCountry":"CHINA","unitValue":5,"itemCount":1,"weight":0,"productURL":"www.ebay.com"},{"itemNo":"00005","sku":"T1818ZS39*1-5","description":"SHOES","nativeDescription":"包含中文字符","hsCode":"TT11","originCountry":"CHINA","unitValue":20,"itemCount":1,"weight":0,"productURL":"www.ebay.com"}]},{"trackingNo":"ABC123456789002","referenceNo":"20160311002","consignmentId":"1","addressLine1":"200 Bumborah Point Rd","addressLine2":"St. Peters","addressLine3":"","city":"PORT BOTANY","country":"AU","description":"SHOES","email":"test@gmail.com","facility":"230031","instruction":"","invoiceCurrency":"AUD","invoiceValue":100,"phone":"","platform":"","postcode":"2036","recipientCompany":"","recipientName":"Bob","serviceOption":"","sku":"T1818ZS39*1","state":"NSW","weightUnit":"","weight":0.776,"dimensionUnit":"","length":0,"width":0,"height":0,"volume":0,"shipperName":"walltech hello","shipperAddressLine1":"pudongxinqu","shipperAddressLine2":"","shipperAddressLine3":"","shipperCity":"","shipperState":"","shipperPostcode":"","shipperCountry":"","shipperPhone":"","recipientTaxId":"","authorityToLeave":"","incoterm":"","lockerService":""},{"referenceNo":"20160311003","addressLine1":"200 Bumborah Point Rd","addressLine2":"St. Peters","addressLine3":"","city":"PORT BOTANY","country":"AU","description":"shoes ","email":"test@gmail.com","facility":"230031","instruction":"","invoiceCurrency":"AUD","invoiceValue":100,"phone":"","platform":"","postcode":"2036","recipientCompany":"","recipientName":"Bob","serviceOption":"","sku":"T1818ZS39*1","state":"NSW","weightUnit":"","weight":0.776,"dimensionUnit":"","length":0,"width":0,"height":0,"volume":0,"shipperName":"walltech hello","shipperAddressLine1":"pudongxinqu","shipperAddressLine2":"","shipperAddressLine3":"","shipperCity":"","shipperState":"","shipperPostcode":"","shipperCountry":"","shipperPhone":"","recipientTaxId":"","authorityToLeave":"","incoterm":"","lockerService":""},{"referenceNo":"20160429004","addressLine1":"200 Bumborah Point Rd","addressLine2":"St. Peters","addressLine3":"","city":"PORT BOTANY","country":"AU","description":"shoes ","email":"test@gmail.com","facility":"230031","instruction":"","invoiceCurrency":"AUD","invoiceValue":100,"phone":"","platform":"","postcode":"2036","recipientCompany":"","recipientName":"Bob","serviceOption":"","sku":"T1818ZS39*1","state":"NSW","weightUnit":"","weight":0.776,"dimensionUnit":"","length":0,"width":0,"height":0,"volume":0,"shipperName":"walltech hello","shipperAddressLine1":"pudongxinqu","shipperAddressLine2":"","shipperAddressLine3":"","shipperCity":"","shipperState":"","shipperPostcode":"","shipperCountry":"","shipperPhone":"","recipientTaxId":"","authorityToLeave":"","incoterm":"","lockerService":""},{"referenceNo":"20160429005","addressLine1":"200 Bumborah Point Rd","addressLine2":"St. Peters","addressLine3":"","city":"PORT BOTANY","country":"AU","description":"shoes ","email":"test@gmail.com","facility":"230031","instruction":"","invoiceCurrency":"AUD","invoiceValue":100,"phone":"","platform":"","postcode":"2036","recipientCompany":"","recipientName":"Jim","serviceOption":"","sku":"T1818ZS39*1","state":"NSW","weightUnit":"","weight":0.776,"dimensionUnit":"","length":0,"width":0,"height":0,"volume":0,"shipperName":"walltech hello","shipperAddressLine1":"pudongxinqu","shipperAddressLine2":"","shipperAddressLine3":"","shipperCity":"","shipperState":"","shipperPostcode":"","shipperCountry":"","shipperPhone":"","returnName":"Wholesale Logisitics","returnAddressLine1":"Unit 17, MIAC Building","returnAddressLine2":"1 international Drive","returnAddressLine3":"","returnCity":"TULLAMARINE","returnState":"VIC","returnPostcode":"3043","returnCountry":"AU","returnOption":"","recipientTaxId":"","authorityToLeave":"","incoterm":"","lockerService":""}]`;
 
-        $body = [];
-
+        $waybill[] = $this->formatOrder($order);
+        echo json_encode($waybill, true);
+        $body = ['body' => $a, "headers" => $this->buildClientHeader("POST", $uri)];
+        var_dump($body);
         $response = $this->client->post($this->host . $uri, $body);
         $result = $this->parseResult($response->getBody());
         $orderResult = new OrderResult();
         if (!empty($result) && strtoupper($result[0]["status"]) == self::SUCCESS) {
-
             $orderResult->expressAgentNumber = $result[0]['referenceNo'];
             $orderResult->expressNumber = $result[0]['orderId'];
             $orderResult->expressTrackingNumber = $result[0]['trackingNo'];
@@ -124,12 +118,33 @@ class EtowerPlatform extends Platform
         // TODO: Implement getOrderAllFee() method.
     }
 
+
+    private function buildClientHeader(string $method, $path): array
+    {
+
+        $t = new \DateTime('now');
+        $t = $t->setTimezone(new \DateTimeZone("GMT+8"));
+
+        $wallTechDate = $t->format(\DateTime::RFC1123);
+        $auth = $method . "\0x000A" . $wallTechDate . "\0x000A" . $this->config->get("host") . $path;
+        $hash = base64_encode(hash_hmac('sha1', $auth, $this->config->get("key"), true));
+        return [
+            'Content-Type' => 'application/json; charset=utf8',
+            "X-WallTech-Date" => $wallTechDate,
+            'Authorization' => sprintf("WallTech %s:%s",
+                $this->config->get("token"),
+                $hash
+            )
+        ];
+    }
+
+
     /**
      * Parse result
      *
-     * @param string $result
+     * @param $result
      * @return array
-     * @throws \Exception
+     * @throws ExpressException
      */
     protected function parseResult($result)
     {
@@ -146,5 +161,84 @@ class EtowerPlatform extends Platform
         }
 
         return $arr['data'];
+    }
+
+    protected function formatOrder(Order $orderClass)
+    {
+        foreach ($orderClass->goods as $key => $value) {
+            $goods[$key] = [
+                "itemNo" => "00001",
+                "sku" => $value->sku,
+                "description" => $value->description,
+                "nativeDescription" => $value->cnDescription,
+                "hsCode" => $value->hsCode,
+                "originCountry" => "", // CHINA
+                "unitValue" => $value->worth,
+                "itemCount" => $value->quantity,
+                "weight" => $value->weight,
+                "productURL" => ""
+            ];
+        }
+
+        return [
+            'trackingNo' => $orderClass->transportCode,
+            'consignmentId' => '',
+            'referenceNo' => $orderClass->customerOrderNo,
+            'addressLine1' => $orderClass->recipient->address,
+            'addressLine2' => '',
+            'addressLine3' => '',
+            'city' => $orderClass->recipient->city,
+            'country' => $orderClass->recipient->countryCode,
+            'description' => $orderClass->package->description,
+            'nativeDescription' => $orderClass->package->description,
+            'email' => $orderClass->recipient->email,
+            'facility' => "",
+            'instruction' => "",
+            'invoiceCurrency' => '',
+            'batteryType' => $orderClass->withBattery,
+            'invoiceValue' => 100,
+            'phone' => $orderClass->recipient->phone,
+            'platform' => "",
+            'postcode' => $orderClass->recipient->zip,
+            'recipientCompany' => $orderClass->recipient->company,
+            'recipientName' => $orderClass->recipient->name,
+            'serviceCode' => '',
+            'serviceOption' => '',
+            "sku" => "",
+            "state" => "",
+            "weightUnit" => "KG",
+            "weight" => $orderClass->package->weight,
+            "dimensionUnit" => "",
+            "length" => $orderClass->package->length,
+            "width" => $orderClass->package->width,
+            "height" => $orderClass->package->height,
+            "volume" => "",
+            "shipperName" => $orderClass->shipper ? $orderClass->shipper->name : "",
+            "shipperAddressLine1" => $orderClass->shipper ? $orderClass->shipper->address : "",
+            "shipperAddressLine2" => "",
+            "shipperAddressLine3" => "",
+            "shipperCity" => $orderClass->shipper ? $orderClass->shipper->city : "",
+            "shipperState" => $orderClass->shipper ? $orderClass->shipper->state : "",
+            "shipperPostcode" => $orderClass->shipper ? $orderClass->shipper->zip : "",
+            "shipperCountry" => $orderClass->shipper ? $orderClass->shipper->countryCode : "",
+            "shipperPhone" => $orderClass->shipper ? $orderClass->shipper->phone : "",
+            "recipientTaxId" => $orderClass->taxesNumber,
+            "authorityToLeave" => "",
+            "incoterm" => "",
+            "lockerService" => "",
+            "extendData" => [
+                "nationalNumber" => "",
+                "nationalIssueDate" => "11/11/2017",
+                "cyrillicName" => "Bob",
+                "imei" => "",
+                "isImei" => true,
+                "vendorid" => "64652016681",
+                "gstexemptioncode" => "",
+                "abnnumber" => "",
+                "sortCode" => "",
+                "coveramount" => 12
+            ],
+            "orderItems" => $goods
+        ];
     }
 }
--- tests/platforms/EtowerPlatformTest.php
+++ tests/platforms/EtowerPlatformTest.php
@@ -8,10 +8,40 @@
  */
 
 namespace platforms;
+
+//require("../../src/Config.php");
+//require("../../src/Express.php");
+//require("../../src/CountryCodes.php");
+//require("../../src/Factory.php");
+//require("../../src/contracts/Order.php");
+//require("../../src/contracts/Goods.php");
+//require("../../src/platforms/PlatformsName.php");
+//require("../../src/platforms/EtowerPlatform.php");
+//require("../../src/platforms/Platform.php");
+
+require_once("../../vendor/autoload.php");
+
 use PHPUnit\Framework\TestCase;
+use yiier\crossBorderExpress\contracts\Goods;
+use yiier\crossBorderExpress\contracts\Order;
+use yiier\crossBorderExpress\contracts\Package;
+use yiier\crossBorderExpress\contracts\Recipient;
+use yiier\crossBorderExpress\contracts\Shipper;
+use yiier\crossBorderExpress\Express;
+use yiier\crossBorderExpress\platforms\PlatformsName;
 
 class EtowerPlatformTest extends TestCase
 {
+    private $config = [
+        "timeout" => 60.0,
+        "platforms" => [
+            PlatformsName::ETOWER_PLATFORM => [
+                "host" => "http://qa.etowertech.com",
+                "token" => "test5AdbzO5OEeOpvgAVXUFE0A",
+                "key" => "79db9e5OEeOpvgAVXUFWSD"
+            ]
+        ]
+    ];
 
     public function testGetTransportsByCountryCode()
     {
@@ -35,7 +65,71 @@ class EtowerPlatformTest extends TestCase
 
     public function testCreateOrder()
     {
+//        $order = new Order();
+//        $order->customerOrderNo = "";
+//        $order->transportCode = "";
+//        $order->goods = [];
+//        $order->evaluate = ""; // 报税
+//        $order->taxesNumber = '91440300MA5DKRQT36'; // 税号
+//        $order->isRemoteConfirm = 1; // 是否同意收偏远费
+//        $order->isReturn = 0; // 是否退件
+//        $order->withBattery = 0; // 是否带电池
 
+        $expressOrder = new Order();
+        $expressOrder->customerOrderNo = 'xxx';
+        $expressOrder->transportCode = 'xxx';
+
+        $goods = new Goods();
+        $goods->description = 'xxx';
+        $goods->cnDescription = 'xxx';
+        $goods->quantity = 'xxx';
+        $goods->weight = 'xxx';
+        $goods->hsCode = 'xxx';
+        $goods->enMaterial = 'cotton';
+        $goods->cnMaterial = '棉';
+        $goods->worth = 1; // 1美元;
+        $goods->sku = 'xxx'; // 云途某些渠道需要
+
+        $expressOrder->goods = [$goods];
+        $expressOrder->evaluate = 1; // 1美元
+        $expressOrder->taxesNumber = 'xxx'; // 税号
+        $expressOrder->isRemoteConfirm = 1; // 是否同意收偏远费
+        $expressOrder->isReturn = 1; // 是否退件
+        $expressOrder->withBattery = 0; // 是否带电池
+
+        $package = new Package();
+        $package->description = 'xxxx';
+        $package->quantity = 1;
+        $package->weight = 'xxx';
+        $package->declareWorth = 1; // 1美元
+        $expressOrder->package = $package;
+
+
+        $recipient = new Recipient();
+        $recipient->countryCode = 'xx';
+        $recipient->name = 'xx';
+        $recipient->address = 'xx';
+        $recipient->city = 'xx';
+        $recipient->state = 'xx';
+        $recipient->zip = 'xx';
+        $recipient->phone = 'xx';
+        $expressOrder->recipient = $recipient;
+
+        $shipper = new Shipper();
+        $shipper->countryCode = 'CN';
+        $shipper->name = 'xxx';
+        $shipper->company = 'xxx';
+        $shipper->address = 'xxx';
+        $shipper->city = 'xxx';
+        $shipper->state = 'xxx';
+        $shipper->zip = 'xx';
+        $shipper->phone = 'xxx';
+        $expressOrder->shipper = $shipper;
+
+        $express = new Express($this->config, PlatformsName::ETOWER_PLATFORM);
+        $orderResult = $express->createOrder($expressOrder);
+
+        var_dump($orderResult);
     }
 
     public function testGetClient()
