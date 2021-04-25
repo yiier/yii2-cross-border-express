388f520795e176932ce8f58fa8cde5bad87b1767
王聪 <solacowa@gmail.com>
2021-04-25 18:46:45 +0800

    add 速递管家

---
+++ src/platforms/Rtb65Platform.php
@@ -0,0 +1,65 @@
+<?php
+
+
+namespace yiier\crossBorderExpress\platforms;
+
+
+use GuzzleHttp\Client;
+use nusoap_client;
+use yiier\crossBorderExpress\contracts\Order;
+use yiier\crossBorderExpress\contracts\OrderFee;
+use yiier\crossBorderExpress\contracts\OrderResult;
+use yiier\crossBorderExpress\contracts\Transport;
+use yiier\crossBorderExpress\exceptions\ExpressException;
+
+class Rtb65Platform extends Platform
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function getClient()
+    {
+        // TODO: Implement getClient() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getTransportsByCountryCode(string $countryCode)
+    {
+        // TODO: Implement getTransportsByCountryCode() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function createOrder(Order $order): OrderResult
+    {
+        // TODO: Implement createOrder() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getPrintUrl(string $orderNumber, array $params = []): string
+    {
+        // TODO: Implement getPrintUrl() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getOrderFee(string $orderNumber): OrderFee
+    {
+        // TODO: Implement getOrderFee() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getOrderAllFee(array $query = []): array
+    {
+        // TODO: Implement getOrderAllFee() method.
+    }
+}
